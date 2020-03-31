7cecc1f72e8b70195909f9f4b4155d8b4c7f567a
goto920 <goto@kmgoto.jp>
2020-04-01 07:31:40 +0900

    new version

---
+++ demos/variableplayer/20200401/precache-manifest.c0f22df3ec5c9420024e9bb3b32aa30f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a25528f3fbaebd2baa754a69ca2c3c9",
+    "url": "/demos/variableplayer/20200401/index.html"
+  },
+  {
+    "revision": "014301e96b38f314e34b",
+    "url": "/demos/variableplayer/20200401/static/css/main.e78db0e4.chunk.css"
+  },
+  {
+    "revision": "d52a20c657305c1221e9",
+    "url": "/demos/variableplayer/20200401/static/js/2.8921b98f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demos/variableplayer/20200401/static/js/2.8921b98f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014301e96b38f314e34b",
+    "url": "/demos/variableplayer/20200401/static/js/main.2be558f9.chunk.js"
+  },
+  {
+    "revision": "9965b3d4a083b4f06d60",
+    "url": "/demos/variableplayer/20200401/static/js/runtime-main.118aa4b7.js"
+  }
+]);
