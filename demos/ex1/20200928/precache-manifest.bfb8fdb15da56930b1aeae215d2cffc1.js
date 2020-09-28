148e949126722fb6a4aa29e3cdb8444d74e02b05
goto920 <goto@kmgoto.jp>
2020-09-28 13:13:22 +0900

    new files ex1

---
+++ demos/ex1/20200928/precache-manifest.bfb8fdb15da56930b1aeae215d2cffc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f281017f8b2f9dc5028c69b8e971b2a6",
+    "url": "/demos/ex1/20200928/index.html"
+  },
+  {
+    "revision": "1ff95147d7410d566699",
+    "url": "/demos/ex1/20200928/static/css/main.6e59662b.chunk.css"
+  },
+  {
+    "revision": "447e9e1d36e34d65be77",
+    "url": "/demos/ex1/20200928/static/js/2.1107b8c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demos/ex1/20200928/static/js/2.1107b8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff95147d7410d566699",
+    "url": "/demos/ex1/20200928/static/js/main.2fe13a3e.chunk.js"
+  },
+  {
+    "revision": "73df5dbe774dae06a892",
+    "url": "/demos/ex1/20200928/static/js/runtime-main.9a23a6c4.js"
+  }
+]);
