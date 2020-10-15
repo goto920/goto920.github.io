8fe7bf13b1eaa0a0a25d3b751c6866b274233b71
goto920 <goto@kmgoto.jp>
2020-10-15 11:01:34 +0900

    add

---
+++ classExamples/toDoList/goal/precache-manifest.05b3eec22f11862910001a9b1d048294.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb662d6fbf4428b5e6827894dc12058",
+    "url": "/classExamples/toDoList/goal/index.html"
+  },
+  {
+    "revision": "ff7e07176d2f71230959",
+    "url": "/classExamples/toDoList/goal/static/css/main.03158f0d.chunk.css"
+  },
+  {
+    "revision": "2dddff81c41333b84eed",
+    "url": "/classExamples/toDoList/goal/static/js/2.9b6ce27b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/classExamples/toDoList/goal/static/js/2.9b6ce27b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7e07176d2f71230959",
+    "url": "/classExamples/toDoList/goal/static/js/main.696f09e8.chunk.js"
+  },
+  {
+    "revision": "5ed4a08133ca8f15bae9",
+    "url": "/classExamples/toDoList/goal/static/js/runtime-main.8e69e89f.js"
+  }
+]);
