e58ef800d61d2cd15bb0f0fbc459720bef4e0a1d
goto920 <goto@kmgoto.jp>
2021-07-11 20:21:59 +0900

    Fix for iPad input type=file accept="audio/*" --> ,.mp3 etc

---
+++ demos/variableplayer/20210711/precache-manifest.d39407e74d371a33e0a85ebca07db45b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cebdd7c2dfaf1b5c9f6c260e0af0c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa4690018064e83d188a",
+    "url": "./static/css/main.e78db0e4.chunk.css"
+  },
+  {
+    "revision": "f1eb1d7d2a86fa1dad3f",
+    "url": "./static/js/2.b542acb0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b542acb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4690018064e83d188a",
+    "url": "./static/js/main.6835955b.chunk.js"
+  },
+  {
+    "revision": "62134353e67d46df10a6",
+    "url": "./static/js/runtime-main.4ec2e254.js"
+  }
+]);
